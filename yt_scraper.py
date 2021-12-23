2a536f0523eb6f8eaba58c3bf2a62117b197daa1
Jason Fong <jasonfong@Jasons-Mac-mini.local>
2021-12-23 14:26:15 -0600

    update

--- yt_scraper.py
+++ yt_scraper.py
@@ -1,6 +1,10 @@
 # importing the libraries
 from bs4 import BeautifulSoup
 import requests
+import json
+import re
+
+
   
 # creating function
 def scrape_info(url):
@@ -13,22 +17,40 @@ def scrape_info(url):
 
     write_file(s.encode())
 
-    print(s.prettify())
+    #list of all <script> tags
+    scripts = s.select("script")
+
+    pattern = re.compile(r"responseContext\s+=\s+(\{.*?\});\n")
+    script = s.find("script", text=pattern)
+    print(script)
+
+  
+    if "responseContext" in scripts[19].string:
+        print("YEPPPPJOASIFJASLFKJ!")
+        json_data =json.loads(scripts[19].string)
+        print(json_data)
+    for i in scripts:
+        if "responseContext" in i.string:
+            print('YEP!!!!!!')
+
+
+
+    # print(s.findAll('script'))
       
-    # finding meta info for title
-    title = s.find("span", class_="watch-title").text.replace("\n", "")
+    # # finding meta info for title
+    # title = s.find("span", class_="watch-title").text.replace("\n", "")
       
-    # finding meta info for views
-    views = s.find("div", class_="watch-view-count").text
+    # # finding meta info for views
+    # views = s.find("div", class_="watch-view-count").text
       
-    # finding meta info for likes
-    likes = s.find("span", class_="like-button-renderer").span.button.text
+    # # finding meta info for likes
+    # likes = s.find("span", class_="like-button-renderer").span.button.text
       
-    # saving this data in dictionary
-    data = {'title':title, 'views':views, 'likes':likes}
+    # # saving this data in dictionary
+    # data = {'title':title, 'views':views, 'likes':likes}
       
     # returning the dictionary
-    return data
+    # return data
 
 def write_file(text):
     # Opening a file
